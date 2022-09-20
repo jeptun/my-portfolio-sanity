21af58508d1e96c0d892febd5dd7e2a4d58d9dd8
jeptun <jeptun@gmail.com>
2022-09-21 01:37:21 +0200

    addstudio

---
+++ studio/schemas/project.js
@@ -0,0 +1,64 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  name: "project",
+  title: "Project",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      type: "string",
+    },
+    {
+      name: "date",
+      type: "datetime",
+    },
+    {
+      name: "place",
+      type: "string",
+    },
+    {
+      name: "description",
+      type: "text",
+    },
+    {
+      name: "projectType",
+      title: "Project type",
+      type: "string",
+      options: {
+        list: [
+          { value: "openSource", title: "Open Source" },
+          { value: "dokumentace", title: "Dokumentace" },
+          { value: "demo", title: "Demo" },
+          { value: "clanek", title: "Článek" },
+          { value: "zdroj", title: "Zdroj" },
+          { value: "kniha", title: "Kniha" },
+          { value: "film", title: "Film" },
+          { value: "hudba", title: "Hudba" },
+          { value: "video", title: "Video" },
+          { value: "tutorial", title: "Tutorial" },
+        ],
+      },
+    },
+    {
+      name: "link",
+      type: "url",
+    },
+    {
+      name: "logoLink",
+      title: "Logo link",
+      type: "url",
+    },
+    {
+      name: "tags",
+      type: "array",
+      of: [
+        {
+          type: "string",
+        },
+      ],
+      options: {
+        layout: "tags",
+      },
+    },
+  ],
+};
